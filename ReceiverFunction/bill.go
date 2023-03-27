1b9f4d31a2b6356bc5264806657cd36d2d713bcf
kaori <kaori55194@gmail.com>
2023-03-26 23:11:44 -0700

    add Receiver function called format

--- ReceiverFunction/bill.go
+++ ReceiverFunction/bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct{
     name string
     items map[string] float64
@@ -14,4 +16,21 @@ func newBill(name string) bill {
 	}
 
 	return b
+}
+
+// receiver function
+func (b *bill)format()string{
+	fs := "bill breakdown: \n"
+	var total float64 = 0
+
+	// list items
+	for k,v := range b.items {
+		fs += fmt.Sprintf("%v ...$%v \n", k+":" , v)
+		total += v
+	}
+
+	// total
+	fs += fmt.Sprintf("%v ...$%0.2f", "total:", total)
+
+	return fs
 }
